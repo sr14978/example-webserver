aaecf4e760629e3886faf0764a05bfbefc835242
Samuel Russell <sr14978@bristol.ac.uk>
2017-10-31 23:23:56 +0000

    added more tests and removed docker tag

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const calc = require('./calc')
 
-if(calc.fib(0) == 0) {
-  process.exit(0)
-} else {
-  console.log("calc.fib(0) != 0")
-  process.exit (1)
+const list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811]
+
+for (var i = 0; i<list.length; i++) {
+  if(calc.fib(i) != list[i]) {
+    console.log('calc.fib('+i+') != '+list[i])
+    process.exit(1)
+  }
 }
+
+process.exit(0)
