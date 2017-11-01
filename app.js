484619db448c9bac1f852e29c1a27e963a679017
Samuel Russell <sr14978@bristol.ac.uk>
2017-11-01 00:03:42 +0000

    fixed varaible

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const calc = require('./calc')
 
 app.get('/', function (req, res) {
   var r = Math.round(Math.random() * 100)
-  res.send('Hi, from Sam. fib(' + r + ') = ' + calc.fib(n))
+  res.send('Hi, from Sam. fib(' + r + ') = ' + calc.fib(r))
 })
 
 app.listen(80, function () {
