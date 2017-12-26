f1756c4d8efd7c6f955a5cfd8709618a1f369101
Samuel Russell <sr14978@bristol.ac.uk>
2017-12-26 02:47:48 +0000

    added path fib

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const calc = require('./calc')
 
 
 
-app.get('/', function (req, res) {
+app.get('/fib', function (req, res) {
   var r = Math.round(Math.random() * 100)
   res.send('Hi, from Sam. fib(' + r + ') = ' + calc.fib(r))
 })
